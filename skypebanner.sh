557c6d94072bee47ac33e1a19e5a59c077cb74c0
Phillip Schichtel <pschichtel@users.noreply.github.com>
2014-03-10 10:45:10 +0100

    added a new proposal

--- skypebanner.sh
+++ skypebanner.sh
@@ -4,6 +4,7 @@
 # 1) envy / puke (by max)
 # 2) pizza / bandit (by armin)
 # 3) puke / mooning (by max)
+# 4) o / drunk (by katharina & phillip)
 
 (echo ''; echo "$@" | figlet -f banner | sed 's/#/(pizza)/g' | sed 's/ /(tumbleweed)/g') | xclip
 echo "$@ skype banner copied to clipboard"
